81c31dc5fcde3369804c74eaf176f086d8bc2947
roy-kugel-nk <kugel.sr@gmail.com>
2013-01-23 18:20:55 +0900

    ytVideoWindow Minor fix. Hide some nodes.

--- forGM/ytVideoWindow/ytVideoWindow.user.js
+++ forGM/ytVideoWindow/ytVideoWindow.user.js
@@ -3,7 +3,7 @@
 // @namespace   ytVideoWindow
 // @description You can use this with 'tile tabs'. Get YouTube embed only.
 // @include     http://www.youtube.com/*
-// @version     1
+// @version     1.01
 // @author      Roy "kugel" nk
 // @grant       none
 // ==/UserScript==
@@ -16,27 +16,28 @@
 	var _e = function(s){
 		return document.getElementById(s);
 	};
+	var _hide = function(s){
+		if ( arguments.length > 0 ) {
+			for (i in arguments) {
+				h = _e(arguments[i]);
+				h.style.display = "none";
+			}
+		}
+	};
 	var _b = document.getElementsByTagName('body').item(0);
 	var _s = {w:screen.availWidth, h:screen.availHeight};
 	var onload = (
 		function(){
-			// Playlist container bar.
-			var c = _e("watch7-playlist-container");
-			c.style.position = "fixed";
-			c.style.top = 0;
-			c.style.left = 0;
+			// Hide unneeded container.
+			_hide("watch7-playlist-container", "watch7-main-container");
 			
-			// Video embeder.
+			// Re-size video embeder.
 			var mp = _e("movie_player");
 			mp.style.position = "fixed";
 			mp.style.top = 0;
 			mp.style.left = 0;
 			mp.style.width = _s.w;
 			mp.style.width = _s.h;
-			
-			// main
-			var main = _e("watch7-main-container");
-			main.style.display = "none";
 		}
 	);
 	onload();
